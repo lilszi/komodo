a3d9d8a6d1a71f7088eeae1a6a4a370aa27aba0a
blackjok3r <blackjok3r@gmail.com>
2019-07-12 10:56:13 +0800

    add customcc to db.h path

--- src/wallet/db.h
+++ src/wallet/db.h
@@ -33,7 +33,7 @@
 
 #include <boost/filesystem/path.hpp>
 
-#ifdef BUILD_ROGUE
+#ifdef BUILD_CUSTOMCC
     #ifdef __APPLE__
         #include "../depends/x86_64-apple-darwin18.6.0/include/db_cxx.h"
     #elif defined(_WIN32)
@@ -41,7 +41,15 @@
     #else
         #include "../depends/x86_64-unknown-linux-gnu/include/db_cxx.h"
     #endif
-#else
+#elif BUILD_ROGUE
+    #ifdef __APPLE__
+        #include "../depends/x86_64-apple-darwin18.6.0/include/db_cxx.h"
+    #elif defined(_WIN32)
+        #include "../depends/x86_64-w64-mingw32/include/db_cxx.h"
+    #else
+        #include "../depends/x86_64-unknown-linux-gnu/include/db_cxx.h"
+    #endif
+#else 
     #include <db_cxx.h>
 #endif
 
