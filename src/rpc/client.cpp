07c243abbe98b71801672312ff8ee7c66ba606e1
blackjok3r <blackjok3r@gmail.com>
2019-08-20 11:29:51 +0800

    rescan from height rpc.

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -110,6 +110,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "importprivkey", 2 },
     { "importprivkey", 3 },
     { "importprivkey", 4 },
+    { "rescanfromheight", 0 },
     { "importaddress", 2 },
     { "verifychain", 0 },
     { "verifychain", 1 },
